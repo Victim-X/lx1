7588f205058fe095dab3c514436eae8976027f93
工具人 <3236156490@qq.com>
2019-08-20 21:54:18 +0800

    登录数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <el-form>
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form :model="formData" :rules="rules" ref="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="formData.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:70%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="formData.code" style="width:70%" placeholder="请输入验证码"></el-input>
           <el-button style="float:right" >发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-            <el-checkbox>我已阅读并同意用户协议和服务条款</el-checkbox>
+        <el-form-item  prop="check">
+            <el-checkbox v-model="formData.check">我已阅读并同意用户协议和服务条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-             <el-button style="width:100%" type="primary">立即登录</el-button>
+             <el-button @click="login" style="width:100%" type="primary">立即登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -24,7 +24,68 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    // 自定义函数用于进行校验
+    var func = function (rule, value, callback) {
+      if (value) {
+        // 如果值满足校验条件
+        // 同意继续向下执行
+        callback()
+      } else {
+        // 如果不满足校验条件
+        // 抛出错误
+        callback(new Error('必须给我钱，懂？'))
+      }
+    }
+    return {
+      formData: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      rules: {
+        mobile: [{
+          required: true,
+          message: '请输入手机号',
+          trigger: 'blur'
+        },
+        // {
+        //   len: 11,
+        //   message: '手机号输入错误',
+        //   trigger: 'blur'
+        // },
+        {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '手机号格式错误'
+          // trigger: 'blur'
+        }],
+
+        code: [{
+          required: true,
+          message: '请输入验证码',
+          trigger: 'blur'
+        }, {
+          pattern: /^\d{6}$/
+        }],
+        check: [{
+          required: true,
+          message: '您必须无条件给我钱',
+          validator: func
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(isOK => {
+        if (isOK) {
+
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
@@ -38,7 +99,7 @@ export default {}
   align-items: center;
   .box-card {
     width: 500px;
-    height: 400px;
+    height: 380px;
 
     .title {
          text-align: center;
