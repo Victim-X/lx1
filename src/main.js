edfee6eff1138e6c6b17656dd8d4aae6f7e660d2
工具人 <3236156490@qq.com>
2019-08-20 22:07:25 +0800

    安装配置axios

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Element from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
+Vue.prototype.$axios = axios// axios插件给了vue对象原型的属性
 Vue.config.productionTip = false
 Vue.use(Element)
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 new Vue({
   router,
   render: h => h(App)
